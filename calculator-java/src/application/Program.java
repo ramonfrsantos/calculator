c071efbc7a96d1b1e7e54c7a90a9618cbe540391
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-08-05 20:39:26 -0300

    locale us pra imprimir área

--- calculator-java/src/application/Program.java
+++ calculator-java/src/application/Program.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import entities.Circle;
 import entities.Rectangle;
@@ -7,6 +8,8 @@ import entities.Shape;
 
 public class Program {
     public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
         
         List<Shape> myShapes = new ArrayList<Shape>();
         myShapes.add(new Rectangle(3.0, 2.0));
@@ -16,7 +19,7 @@ public class Program {
         myCircles.add(new Circle(2.0));
         myCircles.add(new Circle(3.0));
 
-        System.out.println("Total area: " + totalArea(myCircles));
+        System.out.printf("Total area: %.2f", totalArea(myCircles));
     }
 
     // a lista pode ser de Shape ou de qualquer tipo que seja subtipo de Shape (ex: Circle) 
